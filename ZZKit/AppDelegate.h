9fce9404d54f502410a21434a506537af2d7acb4
何宗柱 <hezongzhu@9fbank.cc>
2018-02-04 23:16:56 +0800

    Initial Commit

---
+++ ZZKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZKit
+//
+//  Created by ZongzhuHe on 2018/2/4.
+//  Copyright © 2018年 ZongzhuHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZKit
+//
+//  Created by ZongzhuHe on 2018/2/4.
+//  Copyright © 2018年 ZongzhuHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
