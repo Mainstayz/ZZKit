7f964caba4bda2a6270d66139207d5a57437e4c7
何宗柱 <hezongzhu@9fbank.cc>
2018-02-06 23:01:07 +0800

    add  structure

---
+++ ZZKit/Classes/ZZStructures.h
@@ -0,0 +1,18 @@
+//
+//  ZZStructures_h
+//  ZZKit
+//
+//  Created by ZongzhuHe on 2018/2/6.
+//  Copyright © 2018年 ZongzhuHe. All rights reserved.
+//
+
+#ifndef ZZStructures_h
+#define ZZStructures_h
+
+typedef struct {
+    int64_t tm1;
+    int64_t tm2;
+} ZZStructTime;
+
+
+#endif
